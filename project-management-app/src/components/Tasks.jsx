f05980ab9b07f76baf2490cfafa07d543c806b90
Kowsor Dip <mddip2000@gmail.com>
2026-03-18 02:52:24 +0600

    Implemented Tasks component

--- project-management-app/src/components/Tasks.jsx
+++ project-management-app/src/components/Tasks.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Tasks = ({ projects, projectId }) => {
+  const [tasks, setTasks] = useState([]);
+  const taskRef = useRef();
+
+  const handleTasks = () => {
+    const newTask = {
+      id: Date.now() + "",
+      title: taskRef.current.value,
+    };
+    setTasks((prevTasks) => [newTask, ...prevTasks]);
+    taskRef.current.value = "";
+  };
+
+  const handleClearTask = (taskId) => {
+    setTasks(
+      tasks.filter((element) => {
+        return element.id !== taskId;
+      }),
+    );
+  };
   return (
     <div>
       {projects.map((element) => {
@@ -13,8 +32,22 @@ const Tasks = ({ projects, projectId }) => {
               <hr />
               <div className="tasks">
                 <label>Tasks</label> <br />
-                <input type="text" />
-                <button>Add Task</button>
+                <input type="text" ref={taskRef} />
+                <button onClick={handleTasks}>Add Task</button>
+                <div className="task-list">
+                  <ul>
+                    {tasks.map((element) => {
+                      return (
+                        <li className="each-task" key={element.id}>
+                          {element.title}
+                          <button onClick={() => handleClearTask(element.id)}>
+                            Clear
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
               </div>
             </div>
           );
