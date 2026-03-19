ec7ef8ea7198fc74da50b47a9fc61bbd940dcc53
Kowsor Dip <mddip2000@gmail.com>
2026-03-20 03:32:44 +0600

    Implemented Quiz and Header components

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import React from "react";
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ quiz-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import quizLogo from '../assets/quiz-logo.png'
+
+
+const Header = () => {
+  return (
+    <header>
+        <img src={quizLogo} alt="React quiz logo" />
+        <h1>ReactQuiz</h1>
+    </header>
+  )
+}
+
+export default Header
