11b4beddaa5ac7973f61dc20a2af9841c7d02fc3
Kowsor Dip <mddip2000@gmail.com>
2026-03-20 22:09:35 +0600

    Implemented the functionalities

--- practice-folder/3/src/components/Stopwatch.jsx
+++ practice-folder/3/src/components/Stopwatch.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import "../App.css"
+import React, { useEffect, useRef, useState } from "react";
+import "../App.css";
 
 const Stopwatch = () => {
-  return (
-    <div className='container'>
-      <h1>00:00:00</h1>
-      <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-    </div>
-    </div>
-  )
-}
+  const [timer, setTimer] = useState(0);
 
-export default Stopwatch
+  const totalSeconds = Math.floor(timer/1000);
+
+  const seconds = totalSeconds % 60;
+  const minutes = Math.floor(totalSeconds/60) % 60;
+  const hours = Math.floor(totalSeconds/3600) % 60;
+
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handleStartTimer = () => {
+    setIsRunning(true);
+  };
+  const handleStopTimer = () => {
+    setIsRunning(false);
+  };
+  const handleResetTimer = () => {
+    setIsRunning(false);
+    setTimer(0);
+  };
+
+  const formatTime = (time) => {
+    if(time < 10) {
+       return "0"+time;
+    }
+    return time;
+  }
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1000);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  return (
+    <div className="container">
+      <h1>{formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}</h1>
+      <div className="buttons">
+        <button onClick={handleStartTimer}>Start</button>
+        <button onClick={handleStopTimer}>Stop</button>
+        <button onClick={handleResetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
