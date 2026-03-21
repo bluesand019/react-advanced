f9606e423b04f64fa052e8019a259b448954d0c3
Kowsor Dip <mddip2000@gmail.com>
2026-03-21 20:09:35 +0600

    Added useCallback around handleIncrement and handleDecrement so that they won't be created as a new object each time the props are passed

--- optimization-techniques/src/components/UI/IconButton.jsx
+++ optimization-techniques/src/components/UI/IconButton.jsx
@@ -1,6 +1,10 @@
 import { log } from '../../log.js';
+import { memo } from 'react';
 
-export default function IconButton({ children, icon, ...props }) {
+//the IconButton component is getting re-executed everytime I'm incrementing
+//or decrementing. Thus, we can wrap this with memo to prevent re-executing IconButton and its
+//child components Icon, PlusIcon, MinusIcon
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
@@ -10,4 +14,6 @@ export default function IconButton({ children, icon, ...props }) {
       <span className="button-text">{children}</span>
     </button>
   );
-}
+})
+
+export default IconButton;
