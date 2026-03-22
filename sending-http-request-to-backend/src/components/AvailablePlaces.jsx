aba9a27311f66202bd3f99e501f0b43b1d1c67d4
Kowsor Dip <mddip2000@gmail.com>
2026-03-22 14:37:09 +0600

    Added loading text

--- sending-http-request-to-backend/src/components/AvailablePlaces.jsx
+++ sending-http-request-to-backend/src/components/AvailablePlaces.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     fetch("http://localhost:3000/places")
       .then((response) => {
         return response.json();
       })
       .then((resData) => {
         setAvailablePlaces(resData.places);
+        setIsFetching(false);
       });
   }, []);
 
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading = {isFetching}
+      loadingText = "Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
