a684b3ef3f2876a3f07461fbece3f3ee5b51290e
Kowsor Dip <mddip2000@gmail.com>
2026-03-25 16:34:36 +0600

    Implemented a combined inputChange handler function

--- form-handling/src/components/Login.jsx
+++ form-handling/src/components/Login.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("login!");
-    
+    console.log(`Email: ${enteredValue.email}`);
+    console.log(`Email: ${enteredValue.password}`);
   };
+
+  const handleInputChange = (identifier, event) => {
+    setEnteredValue((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
+  };
+
+  // const handleEmailChange = (event) => {
+  //   setEnteredEmail(event.target.value);
+  // };
+  // const handlePasswordChange = (event) => {
+  //   setEnteredPassword(event.target.value);
+  // };
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -11,20 +36,30 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event)}
+            value={enteredValue.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event)}
+            value={enteredValue.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">
-          Login
-        </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
