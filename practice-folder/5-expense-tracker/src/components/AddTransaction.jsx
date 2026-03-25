a64ca016df13ecf85beaf69853b12d806b862e87
Kowsor Dip <mddip2000@gmail.com>
2026-03-25 23:32:03 +0600

    Refined the UI

--- practice-folder/5-expense-tracker/src/components/AddTransaction.jsx
+++ practice-folder/5-expense-tracker/src/components/AddTransaction.jsx
@@ -5,7 +5,7 @@ const AddTransaction = () => {
     <div className="add-transaction-container">
       <h1>Add Transaction</h1>
       <form className="add-form">
-        <label htmlFor="amount">AMOUNT</label>
+        <label htmlFor="amount" id="amount-label">AMOUNT</label>
         <input type="text" name="amount" id="amount" placeholder="$0.0" />
         <div className="choices">
           <label htmlFor="expense">Expense</label>
