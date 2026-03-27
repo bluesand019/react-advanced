761881b1080ad58890e8cc40e5b9da995cfa0e55
Kowsor Dip <mddip2000@gmail.com>
2026-03-27 19:41:26 +0600

    Added post request to the backend

--- food-order-app/src/components/Checkout.jsx
+++ food-order-app/src/components/Checkout.jsx
@@ -18,12 +18,29 @@ const Checkout = () => {
   function handleClose() {
     userProgessCtx.hideCheckout();
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: ctxValues.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
   return (
     <Modal open={userProgessCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
