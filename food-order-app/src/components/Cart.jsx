c5e718a5428fe2a94fa023aa56188b79ccbf607a
Kowsor Dip <mddip2000@gmail.com>
2026-03-27 18:45:58 +0600

    Improvised cart items

--- food-order-app/src/components/Cart.jsx
+++ food-order-app/src/components/Cart.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import CartContext from "../store/CartContext";
 import Modal from "./UI/Modal";
-import { currencyFormatter } from "../util/formatting"
-import Button from "../components/UI/Button"
+import { currencyFormatter } from "../util/formatting";
+import Button from "../components/UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const ctxValues = useContext(CartContext);
-  const userProgessCtx = useContext(UserProgressContext)
+  const userProgessCtx = useContext(UserProgressContext);
   const cartTotal = ctxValues.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0,
@@ -17,18 +18,25 @@ const Cart = () => {
     userProgessCtx.hideCart();
   }
   return (
-    <Modal className="cart" open={userProgessCtx.progress === 'cart'}>
+    <Modal className="cart" open={userProgessCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {ctxValues.items.map((item) => (
-          <li key={item.id}>
-            {item.name}-{item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => ctxValues.addItem(item)}
+            onDecrease={() => ctxValues.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
