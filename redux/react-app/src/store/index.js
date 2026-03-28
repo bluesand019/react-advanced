b3141581407f3bce06cfa0ff282c94f3e4567897
Kowsor Dip <mddip2000@gmail.com>
2026-03-28 21:31:46 +0600

    created the store

---
+++ redux/react-app/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux'
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increase":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrese":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
