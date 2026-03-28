f4cc5ed3d862c7077dc80f9914e26d47a3480a20
Kowsor Dip <mddip2000@gmail.com>
2026-03-28 20:51:57 +0600

    added action.type

--- redux/core-redux-concepts/redux-demo.js
+++ redux/core-redux-concepts/redux-demo.js
@@ -2,20 +2,30 @@ const redux = require("redux");
 
 //should be a pure function
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
 };
 
-const store = redux.createStore(counterReducer);
+const store = redux.createStore(counterReducer); //initialized store, when initializing the reducer gets called once by the default value
 
-console.log(store.getState());
+console.log(store.getState()); //print the initial default state value
 
 const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
+  const latestState = store.getState(); //get the latest state and store it in latestState
+  console.log(latestState); //print the latest state
 };
 
-store.subscribe(counterSubscriber);
+store.subscribe(counterSubscriber); // run counterSubscriber only when any state changes
 
-store.dispatch({ type: 'increment' });
+store.dispatch({ type: "increment" }); // redux updates state when dispatching an action, then it calls subscriber
+store.dispatch({ type: "decrement" });
