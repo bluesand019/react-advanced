6722ea4e82a5e44784d42a91c72015d85d72ebe4
Kowsor Dip <mddip2000@gmail.com>
2026-03-29 19:16:59 +0600

    Added multiple slices

--- redux/react-app/src/components/Header.js
+++ redux/react-app/src/components/Header.js
@@ -1,6 +1,12 @@
 import classes from './Header.module.css';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store';
 
-const Header = () => {
+// const Header = () => {
+//   const dispatch = useDispatch();
+//    const logoutHandler = () => {
+//     dispatch(authActions.logout());
+//   }
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
--- redux/react-app/src/store/index.js
+++ redux/react-app/src/store/index.js
@@ -1,13 +1,13 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { createSlice, configureStore, isAction } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increase(state) {
       //we can mutate states in here because redux toolkit will automatically
@@ -26,11 +26,27 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
 
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
