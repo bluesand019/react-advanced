db4d2b2e4272d2e9d3f0477c3a2df1e776c45a30
Kowsor Dip <mddip2000@gmail.com>
2026-04-04 22:34:03 +0600

    Added header

---
+++ practice-folder/7/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-blue-500 text-white w-full p-4 text-center text-2xl font-bold">
+      My React App
+    </header>
+  );
+}
