7173510f4f5ab122d1c95de70756ec9f1c62d1cf
Kowsor Dip <mddip2000@gmail.com>
2026-04-04 22:34:34 +0600

    added footer

---
+++ practice-folder/7/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white w-full p-3 text-center">
+      © 2026 My App
+    </footer>
+  );
+}
